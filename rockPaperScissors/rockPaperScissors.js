15154a10e6d94c25111465fab5e80969ccab955c
unknown <nouhachorfi2@gmail.com>
2020-03-21 11:07:51 +0100

    rockPaperScissors notFinish Nouha

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,7 +18,23 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
-};
+var rockPaperScissors = function () {
+  var st=""
+  var str1="RRR"
+  var str2="PPP"
+  var str3="SSS"
+  var result=[]
+  for(var i=0 ; i<str1.length; i++){
+    st=str1[i]
+    for(var j=0 ; j<str2.length; j++){
+        st=st+str2[j]
+      for(var k=0 ; k<str3.length; k++){
+        st=st+str3[k]
+        result.push(st)
+      }
+
+    } 
+  }
+  return result
+
+  }
