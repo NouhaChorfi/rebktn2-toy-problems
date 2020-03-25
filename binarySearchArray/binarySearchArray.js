2aadbd2c5c1998964364e8a62240fdba55e6c5c8
unknown <nouhachorfi2@gmail.com>
2020-03-25 11:39:30 +0100

    binarySearchArray Nouha

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,26 @@
  */
 
 var binarySearch = function (array, target) {
-};
+    var firstIndex  = 0,
+        lastIndex   = array.length - 1,
+        middleIndex = Math.floor((lastIndex + firstIndex)/2);
 
+    while(array[middleIndex] != target && firstIndex < lastIndex)
+    {
+       if (target < array[middleIndex])
+        {
+            lastIndex = middleIndex - 1;
+        } 
+      else if (target > array[middleIndex])
+        {
+            firstIndex = middleIndex + 1;
+        }
+        middleIndex = Math.floor((lastIndex + firstIndex)/2);
+    }
+
+      if (array[middleIndex] != target) {
+         return null
+     }
+      else{ return middleIndex;}
+
+};
