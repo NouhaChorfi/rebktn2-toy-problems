6ab081bb581f739eefd27d07d0436aab19a2c793
unknown <nouhachorfi2@gmail.com>
2020-04-03 10:36:40 +0100

    balanced parantheses Nouha

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,22 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var paratheses=str.match(/\(|\)/g)
+    for (var i=0 ; i< paratheses.length ; i++){
+        if(/\)/.test(paratheses[i])){
+            if(/\(/.test(paratheses[i-1])){
+                paratheses.splice(i-1,2)
+                i=i-2
+            }
+            if(/\)/.test(paratheses[0])){
+               return false
+            }
+        }
+    }
+   if( paratheses.length>0){
+       return false
+    }
+    return true
+
+}
