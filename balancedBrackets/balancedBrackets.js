d26e583c7f7407f48535cd6b3092eb87c0b85008
unknown <nouhachorfi2@gmail.com>
2020-04-08 10:11:08 +0100

    balanced Brakets Nouha

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,38 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var strBrakets=[]
+    var brakets={
+        "{":"}",
+        "[":"]",
+        "(":")",
+        '"':'"',
+    }
+    var closed={
+        "}":"}",
+        "]":"]",
+        ")":")",
+        '"':'"'
+    }
+    for (var i=0 ; i<str.length ; i++){
+        if(str[i] in brakets || str[i] in closed){
+            strBrakets.push(str[i])
+        }
+    }
+    for (var i=0 ; i<strBrakets.length ; i++){
+        if( strBrakets[i] in closed){
+            if(brakets[strBrakets[i-1]]=== strBrakets[i]){
+                strBrakets.splice(i-1, 2);
+                i=i-2
+            }else{
+                return false
+            }
+        }
+    }
+    if(strBrakets.length === 0){
+        return true
+    }
+    return false
+
+};
